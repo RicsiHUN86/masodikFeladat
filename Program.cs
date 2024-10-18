928a0047bfc6ea1d8271a4fe2f69f0a8f7b9f8c7
BRicsi <barnusrichi@gmail.com>
2024-10-18 10:28:46 +0200

    :)

--- Program.cs
+++ Program.cs
@@ -10,6 +10,28 @@ namespace masodikFeladat
     {
         static void Main(string[] args)
         {
+            {
+                int pozitivOsszeg = 0;
+                int negativOsszeg = 0;
+
+                for (int i = 0; i < 10; i++)
+                {
+                    Console.Write("Add meg a {0}. számot: ", i + 1);
+                    int szam = int.Parse(Console.ReadLine());
+
+                    if (szam > 0)
+                    {
+                        pozitivOsszeg += szam;
+                    }
+                    else if (szam < 0)
+                    {
+                        negativOsszeg += szam;
+                    }
+                }
+
+                Console.WriteLine("Pozitív számok összege: " + pozitivOsszeg);
+                Console.WriteLine("Negatív számok összege: " + negativOsszeg);
+            }
         }
     }
 }
